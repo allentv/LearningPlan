b64cf3d318e0e0cbae423bd9ff57269b0ccf2c5d
Allen Thomas Varghese <allentv4u@gmail.com>
2017-05-10 22:40:22 +0100

    Minor configuration updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
-const path = require('path');
-var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require("path");
+var webpack = require("webpack");
+var CompressionPlugin = require("compression-webpack-plugin");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  devtool: 'eval',
+  devtool: "eval",
   devServer: {
     hot: true,
-    contentBase: path.join(__dirname, "dist"),
     compress: true,
     port: 10000
   },
@@ -16,23 +16,61 @@ module.exports = {
   },
   output: {
     filename: "bundle.js",
-    path: __dirname + "dist",
-    chunkFilename: '[id].[chunkhash].js'
+    path: __dirname + "/dist",
+    chunkFilename: "[name].[chunkhash].js"
   },
   resolve: {
     extensions: [".js", ".jsx", ".json"],
-    modules: [path.resolve(__dirname, "src/components"), "node_modules"]
+    modules: [
+      path.resolve(__dirname, "src/components"),
+      "node_modules"
+    ]
   },
   plugins: [
-    new ExtractTextPlugin('styles.css'),
+    new webpack.DefinePlugin({
+      "process.env": {
+        "NODE_ENV": JSON.stringify("production")
+      }
+    }),
+    new ExtractTextPlugin("styles.css"),
     new HtmlWebpackPlugin({
       inject: true,
-      hash: true
+      hash: true,
+      title: "Learning Plan"
     }),
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      comments: false,
+      compress: {
+        warnings: false,
+        unused: true,
+        dead_code: true,
+        drop_debugger: true,
+        conditionals: true,
+        evaluate: true,
+        drop_console: true,
+        sequences: true,
+        booleans: true
+      }
+    }),
+    new CompressionPlugin({
+      asset: "[path].gz[query]",
+      algorithm: "gzip",
+      test: /\.js$/,
+      threshold: 10240,
+      minRatio: 0.8
+    })
   ],
   module: {
     loaders: [
+      {
+        test: /\.(jpg|jpeg|gif|png)$/,
+        loader: "file-loader?name=img/[path][name].[ext]"
+      },
+      {
+        test: /\.(eof|woff|woff2|svg)$/,
+        loader: "file-loader?name=img/[path][name].[ext]"
+      },
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
@@ -50,7 +88,7 @@ module.exports = {
             {
               loader: "css-loader",
               options: {
-                sourceMap: true,
+                sourceMap: false,
                 modules: true,
                 importLoaders: 1
               }
@@ -58,6 +96,6 @@ module.exports = {
           ]
         })
       }
-    ],
+    ]
   }
 }
